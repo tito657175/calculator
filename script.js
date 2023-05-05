f098be7b6a814714ff5049ad4d277a195ff64485
tito <tito657175>
2023-05-04 21:47:20 -0700

    Add separate functions for various operators as well as test way to store numbers

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+//Global Variables
+    const operators = ["+","-","/","*"];
+    let selectedButton = 0;
+
+//Initial event listener
 function onClick(button){
-    console.log(button);
+    if (operators.includes(button)){
+        console.log (`U are using this ${button} operator`);
+    } else if (button == "=") {
+        return equals();
+    } else if (button == "C"){
+        return clear();
+    } else {
+        selectedButton += button;
+        return document.getElementById("display").innerHTML = selectedButton;
+    }
+}
+
+function equals(){
+    console.log("equals ya fu");
+}
+
+function clear(){
+    console.log("clear!")
 }
