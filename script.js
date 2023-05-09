a1c88e8c1f0f10dadbef9415c2301708c3700697
tito <tito657175>
2023-05-09 14:47:39 -0700

    No code, make a speudo code layout to work though some of the logic

--- script.js
+++ script.js
@@ -99,3 +99,53 @@ function resetDefault(){
     secondNumber = 0;
     total = 0;
 }
+
+/* come back later to completely rework code
+goal is to make the calculations work as follows
+
+1. button = first numbers
+    //for step 5 in case type number after total
+    if total = firstNumber
+        firstNumber = button
+        display update
+    // for first operator ever
+    if operator (firstOperator == null)
+        return (do nothing)
+
+2. button = first operator
+        if secondNumber  == null
+            skip calculate()
+        button = first operator
+
+3. button = second numbers
+    if button = operator
+        skips calculation() cuz second still = null
+        button = first operator (replace existing operator on display)
+
+4. choices (now have first, operator, second)(calculate totals)
+    if operator,
+        calculate(a,firstOperator,b)
+            total = first number (first operator) second number
+        firstNumber = total
+        button = firstOperator (replace to new firstOperator)
+
+    if equals,
+        calculate total
+            select which operator function to use
+                total = first number (first operator) second number
+        firstNumber = total
+        first operator = null
+        secondNumber = null
+       *** display = firstNumber (no longer running total)
+    
+5.(NOT HAVE TOTAL HERE)
+    if number
+        due 3. button = secondNumber
+
+    if operator
+        due 2. button = first operator
+            replaces previous operator type
+6. revert to 
+    4. choices
+        repeat the loop as needed
+*/
